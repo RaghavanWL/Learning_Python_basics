5b3fa806546b4a4b2ebd72130544a23d76978909
Sai Prahalatha Raghavan .V <raghavanmarch2002@gmail.com>
2020-12-10 22:56:20 +0530

    Added new file

---
+++ URL opening.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+print("Do you want to hear my favorite music ?")
+list_answer = ["Y", "YES", "N", "NO"]
+input_answer = input("(Yes/No): ").upper()
+if input_answer in list_answer:
+    if (input_answer == "YES") or (input_answer == "Y"):
+        webbrowser.open("https://youtu.be/UOxkGD8qRB4")
+else:
+    print("option not available")
--- guess game.py
+++ guess game.py
@@ -1,13 +1,13 @@
 import random
-stert_value=int(input("please enter the start value : "))
-end_value=int(input("please enter the end value : "))
+start_value = int(input("please enter the start value : "))
+end_value = int(input("please enter the end value : "))
 print(" ")
-ans=random.randint(stert_value,end_value)
+ans = random.randint(start_value, end_value)
 b = int(input("guess a number : "))
 print(" ")
 if b == ans:
     print("wow you guessed it first time !")
-elif (b!= ans)and(b!=0)and(stert_value<=b<=end_value):
+elif (b != ans) and (b != 0) and (start_value <= b <= end_value):
     b = 0
     while b != ans:
         b = int(input("guess again !: "))
@@ -22,7 +22,7 @@ elif (b!= ans)and(b!=0)and(stert_value<=b<=end_value):
                 print("please guess a higher number !")
         else:
             print("finally you got it correct !")
-elif b==0:
+elif b == 0:
     print("meet you again ! bye!!!")
 else:
     print("please guess within the range !")
