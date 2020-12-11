12f51113874d604a1d57b6fad4beceabdddeca5e
Sai Prahalatha Raghavan .V <raghavanmarch2002@gmail.com>
2020-12-11 08:45:33 +0530

    Added new link

--- URL opening.py
+++ URL opening.py
@@ -6,5 +6,9 @@ input_answer = input("(Yes/No): ").upper()
 if input_answer in list_answer:
     if (input_answer == "YES") or (input_answer == "Y"):
         webbrowser.open("https://youtu.be/UOxkGD8qRB4")
+        print("want another one ?")
+        input_answer = input("(Yes/No): ").upper()
+        if (input_answer == "YES") or (input_answer == "Y"):
+            webbrowser.open("https://youtu.be/RkID8_gnTxw")
 else:
     print("option not available")
