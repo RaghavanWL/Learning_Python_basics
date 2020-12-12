9cf5b4975717ffbf10716abd15432f2f5cefe633
Sai Prahalatha Raghavan .V <raghavanmarch2002@gmail.com>
2020-12-12 23:08:49 +0530

    new game

---
+++ Reaction Time game.py
@@ -0,0 +1,36 @@
+# Please install keyboard module to run this program. 
+# Type "pip install keyboard" in command prompt to install.
+
+import time
+import keyboard
+import random
+g = random.randint(3,5)
+print("i will say ready steady go when i say go press enter")
+print()
+print()
+time.sleep(1)
+print("READY")
+print()
+print()
+time.sleep(1)
+print("STEADY")
+print()
+print()
+time.sleep(g)
+print("=X=X=X= GO =X=X=X=")
+startingTime = time.time()
+while True:
+    if keyboard.is_pressed("enter"):
+        endingTime = time.time()
+        break
+print()
+print()
+reactionTime = endingTime-startingTime
+if reactionTime == 0.13029383659362793:
+    print("IMPOSIBLE !!! YOU AND ME HAVE THE SAME REACTION TIME")
+else: 
+    print("Your reaction time is : ",reactionTime)
+if reactionTime <= 0.1:
+    print()
+    print()
+    print("WOW ! you have a very good reaction time")
