52abba6c097a6626480ef5f5850ddf30a4ce4796
Sai Prahalatha Raghavan .V <raghavanmarch2002@gmail.com>
2020-12-17 16:08:45 +0530

    time module

--- My Favorite song.py
+++ My Favorite song.py
@@ -1,14 +1,13 @@
 import webbrowser
+import time
 
-print("Do you want to hear my favorite music ?")
+print("Do you want to hear my favorite musics ?")
 list_answer = ["Y", "YES", "N", "NO"]
 input_answer = input("(Yes/No): ").upper()
 if input_answer in list_answer:
     if (input_answer == "YES") or (input_answer == "Y"):
         webbrowser.open("https://youtu.be/UOxkGD8qRB4")
-        print("want another one ?")
-        input_answer = input("(Yes/No): ").upper()
-        if (input_answer == "YES") or (input_answer == "Y"):
-            webbrowser.open("https://youtu.be/RkID8_gnTxw")
+        time.sleep(205)
+        webbrowser.open("https://youtu.be/RkID8_gnTxw")
 else:
     print("option not available")
